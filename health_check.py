df204a09b30461e98b4dc130b01462c17a4e8da6
Joaquin Escobar Figueredo <drkzzzz@Laptop-de-Joaquin.local>
2024-09-27 10:45:04 -0300

    Script Monitor de Recursos

---
+++ health_check.py
@@ -0,0 +1,87 @@
+import psutil
+import platform
+import subprocess
+import subprocess
+
+
+def get_os_info():
+    os_name = platform.system()
+    return os_name
+
+
+os_info = get_os_info()
+
+
+def health_check():
+    # Información de CPU
+    cpu_usage = psutil.cpu_percent(interval=1)
+
+    # Información de Memoria RAM
+    ram = psutil.virtual_memory()
+    print(ram)
+    ram_usage = ram.percent
+    ram_total_gb = ram.total / 1e9
+    ram_available_gb = ram.available / 1e9
+    ram_used_gb = ram.used / 1e9
+    ram_free_gb = ram.free / 1e9
+    ram_active_gb = ram.active / 1e9
+    ram_inactive_gb = ram.inactive / 1e9
+
+    # Crear un diccionario con la información
+    health_info = {
+        "cpu": {
+            "usage_percentage": cpu_usage,
+        },
+        "ram": {
+            "usage_percentage": ram_usage,
+            "total_gb": ram_total_gb,
+            "available_gb": ram_available_gb,
+            "used_gb": ram_used_gb,
+            "free_gb": ram_free_gb,
+            "active_gb": ram_active_gb,
+            "inactive_gb": ram_inactive_gb
+        },
+    }
+
+    # Información de Disco
+    # Ejecutar disk.py para tomar la información de las particiones correspondientes
+    if os_info == "Windows":
+        disk = psutil.disk_usage('/System/Volumes/Data')
+        disk_usage = disk.percent
+        disk_total_gb = disk.total / 1e9  # Convertir a GB
+        disk_used_gb = disk.used / 1e9
+        disk_free_gb = disk.free / 1e9
+        disk = {"disk": {"usage_percentage": disk_usage,
+                         "total_gb": disk_total_gb,
+                         "used_gb": disk_used_gb,
+                         "free_gb": disk_free_gb}}
+        health_info.update(disk)
+    elif os_info == "Darwin":
+        disk = psutil.disk_usage('/System/Volumes/Data')
+        disk_usage = disk.percent
+        disk_total_gb = disk.total / 1e9  # Convertir a GB
+        disk_used_gb = disk.used / 1e9
+        disk_free_gb = disk.free / 1e9
+        disk = {"disk": {"usage_percentage": disk_usage,
+                         "total_gb": disk_total_gb,
+                         "used_gb": disk_used_gb,
+                         "free_gb": disk_free_gb}}
+        health_info.update(disk)
+    return health_info
+
+
+def print_health_info(info):
+    print("=== Información del Sistema ===")
+    for component, metrics in info.items():
+        print(f"\n{component}:")
+        for metric, value in metrics.items():
+            # Imprimir el valor con dos decimales, excepto para el porcentaje
+            if 'percentage' in metric:
+                print(f"  {metric}: {value:.2f}%")
+            else:
+                print(f"  {metric}: {value:.2f} GB")
+
+
+if __name__ == "__main__":
+    health_info_result = health_check()
+    print_health_info(health_info_result)
